229fe2b4d1aa755ac573cfd38a4376f42629c787
harrisb002 <harrisbe@somoma.edu>
2023-08-16 21:34:29 -0700

    Adding mongoose connection/schema

---
+++ models/Product.js
@@ -0,0 +1,12 @@
+const { model, Schema, models, default: mongoose } = require("mongoose");
+
+const ProductSchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  category: { type: mongoose.Types.ObjectId, ref: "Category" },
+  properties: { type: Object },
+  images: [{ type: String }],
+});
+
+export const Product = models.Product || model("Product", ProductSchema);
