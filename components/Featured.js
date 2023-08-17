3f85f7291469ea5a310950d28f6eeee48d146a48
harrisb002 <harrisbe@somoma.edu>
2023-08-16 19:08:06 -0700

    Styling featured product

--- components/Featured.js
+++ components/Featured.js
@@ -2,19 +2,29 @@ import styled from "styled-components";
 import Center from "./Center";
 
 const Bg = styled.div`
-  background-color: #22;
+  background-color: #222;
+  color: #fff;
+  padding: 50px 0;
+`;
+const Title = styled.h1`
+  margin: 0;
+  font-width: normal;
+`;
+const Desc = styled.p`
+  color: #aaa;
+  font-size: 0.8rem;
 `;
 
 export default function Featured() {
   return (
-    <div>
+    <Bg>
       <Center>
-        <h1>Featured Product</h1>
-        <p>
+        <Title>Featured Product</Title>
+        <Desc>
           This is an Awesome pink turtle with an attached chain for your keys or
           to be hanged anywhere you would like.
-        </p>
+        </Desc>
       </Center>
-    </div>
+    </Bg>
   );
 }
