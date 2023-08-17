7a3e853dc88036dbe73283bd23a84c311bfe4b7d
harrisb002 <harrisbe@somoma.edu>
2023-08-16 21:27:18 -0700

    Styling featured product buttons

--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,66 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledButton = styled.button`
-  background-color: #23d9f8;
   border: 0;
-  color: #000;
   padding: 5px 15px;
   border-radius: 5px;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  text-decoration: none;
+  font-weight: 500;
+  svg {
+    height: 16px;
+    margin-right: 5px;
+  }
+  ${(props) =>
+    props.block &&
+    css`
+      display: block;
+      width: 100%;
+    `}
+  ${(props) =>
+    props.white &&
+    !props.outline &&
+    css`
+      background-color: #fff;
+      color: #000;
+    `}
+  ${(props) =>
+    props.white &&
+    props.outline &&
+    css`
+      background-color: transparent;
+      color: #fff;
+      border: 1px solid #fff;
+    `}
+  ${(props) =>
+    props.primary &&
+    !props.outline &&
+    css`
+      background-color: #19a6c0;
+      border: 1px solid #19a6c0;
+      color: #fff;
+    `}
+  ${(props) =>
+    props.primary &&
+    props.outline &&
+    css`
+      background-color: transparent;
+      border: 1px solid #19a6c0;
+      color: #19a6c0;
+    `}
+  ${(props) =>
+    props.size === "l" &&
+    css`
+      font-size: 1.2rem;
+      padding: 10px 20px;
+      svg {
+        height: 20px;
+      }
+    `}
 `;
 
-export default function Button({ children }) {
-  return <StyledButton>{children}</StyledButton>;
+export default function Button({ children, ...rest }) {
+  return <StyledButton {...rest}>{children}</StyledButton>;
 }
--- components/Featured.js
+++ components/Featured.js
@@ -10,18 +10,24 @@ const Bg = styled.div`
 const Title = styled.h1`
   margin: 0;
   font-width: normal;
+  font-size: 3rem;
 `;
 const Desc = styled.p`
   color: #aaa;
   font-size: 0.8rem;
 `;
-const Wrapper = styled.div`
+const ColumnsWrapper = styled.div`
   display: grid;
   grid-template-columns: 0.9fr 1fr;
   gap: 40px;
   img {
     max-width: 100%;
   }
+`;
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 12px;
+  margin-top: 25px;
 `;
 
 const Column = styled.div`
@@ -33,7 +39,7 @@ export default function Featured() {
   return (
     <Bg>
       <Center>
-        <Wrapper>
+        <ColumnsWrapper>
           <Column>
             <div>
               <Title>Featured Product</Title>
@@ -41,8 +47,22 @@ export default function Featured() {
                 This is an Awesome pink turtle with an attached chain for your
                 keys or to be hanged anywhere you would like.
               </Desc>
-              <Button>Read more</Button>
-              <Button>Add to cart</Button>
+              <ButtonsWrapper>
+                <Button outlined white>
+                  Read more
+                </Button>
+                <Button primary>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                  </svg>
+                  Add to cart
+                </Button>
+              </ButtonsWrapper>
             </div>
           </Column>
           <Column>
@@ -51,7 +71,7 @@ export default function Featured() {
               alt=""
             />
           </Column>
-        </Wrapper>
+        </ColumnsWrapper>
       </Center>
     </Bg>
   );
