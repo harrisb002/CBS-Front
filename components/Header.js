962bb134422700ebd12eb6d22b9df05d0639ec9c
harrisb002 <harrisbe@somoma.edu>
2023-08-16 17:33:03 -0700

    Adding style for header

--- components/Header.js
+++ components/Header.js
@@ -2,27 +2,33 @@ import Link from "next/link";
 import styled from "styled-components";
 import Center from "./Center";
 
+//Styled Components
 const StyledHeader = styled.header`
   background-color: #222;
 `;
-
 const Logo = styled(Link)`
   color: #fff;
   text-decoration: none;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 export default function Header() {
   return (
     <StyledHeader>
       <Center>
-        <Logo href={"/"}>CBS</Logo>
-        <nav>
-          <Link href={"/"}>Home</Link>
-          <Link href={"/products"}>All Products</Link>
-          <Link href={"/categories"}>Categories</Link>
-          <Link href={"/account"}>Account</Link>
-          <Link href={"/cart"}>Cart</Link>
-        </nav>
+        <Wrapper>
+          <Logo href={"/"}>CBS</Logo>
+          <nav>
+            <Link href={"/"}>Home</Link>
+            <Link href={"/products"}>All Products</Link>
+            <Link href={"/categories"}>Categories</Link>
+            <Link href={"/account"}>Account</Link>
+            <Link href={"/cart"}>Cart</Link>
+          </nav>
+        </Wrapper>
       </Center>
     </StyledHeader>
   );
