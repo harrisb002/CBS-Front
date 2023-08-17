9a82e1b20f3a8c49fd9b20bda8365da814ffc132
harrisb002 <harrisbe@somoma.edu>
2023-08-16 17:30:22 -0700

    Adding next config

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ const GlobalStyles = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
   body{
     padding:0;
-    margin:0
+    margin:0;
     font-family: 'Roboto', sans-serif;
   }
 `;
